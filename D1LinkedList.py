ed8dbeef7a3e4ea3b370464ff9f444e283737f29
B HariKrishna reddy <45511185+BHariKrishnaReddy@users.noreply.github.com>
2022-01-01 23:45:58 +0530

    Create D1LinkedList.py

---
+++ D1LinkedList.py
@@ -0,0 +1,81 @@
+#to creat the LL we need two class Node and SingleLinkedList
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next = None
+
+class SingleLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+  
+  #Insertion to LinkedList
+  def insert(self,data,position):
+    newNode = Node(data)
+    if self.head == None:
+      self.head = newNode
+      self.tail = newNode
+    elif position == 1:
+      #if position is 1 it refers that insertion at Head
+      newNode.next = self.head
+      self.head = newNode
+    elif position == -1:
+      #if position is -1 it refers that insertion at Tail
+      self.tail.next = newNode
+      self.tail = newNode
+    else:
+      #if it in between we need travers thru the list and insert it
+      temp,index = self.head,0
+      while index < position -1 :
+        temp = temp.next
+        index+=1
+      nextnode=temp.next
+      temp.next=newNode
+      newNode.next=nextnode
+
+  def delete(self,position):
+    if self.head==None:
+      return "Empty Linked list"
+    elif position == 0:
+      self.head= self.head.next
+    elif position == -1:
+      temp=self.head
+      while temp is not None:
+        if temp.next==self.tail:
+          break
+        temp=temp.next
+      temp.next=None
+      self.tail=temp
+    else:
+      temp,i=self.head,0
+      while i<position-1:
+        temp=temp.next
+        i+=1
+      nextnode = temp.next
+      temp.next=n.next
+
+  #Travesal
+  def travesel(self):
+    if self.head==None:
+      return "Empty list"
+    else:
+      temp=self.head
+      while temp is not None:
+        print(temp.value)
+        temp=temp.next
+
+  #itertion - can use to print the List.
+  def _iter_(self):
+    temp= self.head
+    while temp:
+      yield temp
+      temp=temp.next
+
+  
+sl = SingleLinkedList()
+sl.delete(3)
+sl.insert(10,1)
+sl.insert(11,2)
+sl.insert(12,4)
+sl.insert(13,4)
+
