9e8146ae8ace8934cdafab43b97e65cd1492756f
B HariKrishna reddy <45511185+BHariKrishnaReddy@users.noreply.github.com>
2022-01-02 18:43:11 +0530

    LinkedList with all basic operations.

--- D1LinkedList.py
+++ D1LinkedList.py
@@ -1,83 +1,108 @@
-#to creat the LL we need two class Node and SingleLinkedList
 class Node:
-  def __init__(self,data):
-    self.data=data
-    self.next = None
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
 
-class SingleLinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
-  
-  #Insertion to LinkedList
-  def insert(self,data,position):
-    newNode = Node(data)
-    if self.head == None:
-      self.head = newNode
-      self.tail = newNode
-    elif position == 1:
-      #if position is 1 it refers that insertion at Head
-      newNode.next = self.head
-      self.head = newNode
-    elif position == -1:
-      #if position is -1 it refers that insertion at Tail
-      self.tail.next = newNode
-      self.tail = newNode
-    else:
-      #if it in between we need travers thru the list and insert it
-      temp,index = self.head,0
-      while index < position -1 :
-        temp = temp.next
-        index+=1
-      nextnode=temp.next
-      temp.next=newNode
-      newNode.next=nextnode
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
 
-  def delete(self,position):
-    if self.head==None:
-      return "Empty Linked list"
-    elif position == 0:
-      self.head= self.head.next
-    elif position == -1:
-      temp=self.head
-      while temp is not None:
-        if temp.next==self.tail:
-          break
-        temp=temp.next
-      temp.next=None
-      self.tail=temp
-    else:
-      temp,i=self.head,0
-      while i<position-1:
-        temp=temp.next
-        i+=1
-      nextnode = temp.next
-      temp.next=nextnode.next
+    def insertSLL(self, value, location):
+            newNode = Node(value)
+            if self.head is None:
+                self.head = newNode
+                self.tail = newNode
+            else:
+                if location == 0:
+                    newNode.next = self.head
+                    self.head = newNode
+                elif location == -1:
+                    newNode.next = None
+                    self.tail.next = newNode
+                    self.tail = newNode
+                else:
+                    tempNode = self.head
+                    index = 0
+                    while index < location - 1:
+                        tempNode = tempNode.next
+                        index += 1
+                    nextNode = tempNode.next
+                    tempNode.next = newNode
+                    newNode.next = nextNode
+                    if tempNode == self.tail:
+                        self.tail=newNode
+    # Traverse Singly Linked List
+    def traverseSLL(self):
+        if self.head is None:
+            print("The Singly Linked List does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    # Search for a node in Singly Linked List
+    def searchSLL(self, nodeValue):
+        if self.head is None:
+           return "The list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.value
+                node = node.next
+            return "The value does not exist in this list"
 
-  #Travesal
-  def travesel(self):
-    if self.head==None:
-      return "Empty list"
-    else:
-      temp=self.head
-      while temp is not None:
-        print(temp.value)
-        temp=temp.next
-
-  #itertion - can use to print the List.
-  def _iter_(self):
-    temp= self.head
-    while temp:
-      yield temp
-      temp=temp.next
-      
-  #Delete entire LinkedList    
-  def deleteEntireSLL(self):
+    #  Delete a node from Singly Linked List
+    def deleteNode(self, location):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+    # Delete entire SLL
+    def deleteEntireSLL(self):
         if self.head is None:
             print("The SLL does not exist")
         else:
             self.head = None
             self.tail = None
 
-  
-sl = SingleLinkedList()
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+singlyLinkedList.insertSLL(5,-1)
+
+print([node.value for node in singlyLinkedList]) 
