50bad1af2fc242db2198a8897db8fa788b13e127
BHariKrishnaReddy <reddyfrn007@gmail.com>
2022-10-13 23:11:19 +0530

    reformatted

--- Basics/D6Tree(Pre,In,Post).py
+++ Basics/D6Tree(Pre,In,Post).py
@@ -1,17 +1,18 @@
 # Run in your local for more clarity of the program
 class TreeNode:
-  def __init__(self,data):
-    self.data = data
-    self.left = None
-    self.right = None
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-def preOrder(root): #root ->left ->right
-  if not root:
-    return
-  print(root.data)
-  preOrder(root.left)
-  preOrder(root.right)
-  '''
+
+def preOrder(root):  # root ->left ->right
+    if not root:
+        return
+    print(root.data)
+    preOrder(root.left)
+    preOrder(root.right)
+    '''
   For this the following output will be like...
   --Prorder--
 
@@ -24,13 +25,14 @@ def preOrder(root): #root ->left ->right
             R - RightChild
   '''
 
-def inOrder(root): #left-> root ->right
-  if not root:
-    return
-  preOrder(root.left)
-  print(root.data)
-  preOrder(root.right)
-  '''
+
+def inOrder(root):  # left-> root ->right
+    if not root:
+        return
+    preOrder(root.left)
+    print(root.data)
+    preOrder(root.right)
+    '''
   --Inorder--
 
           LeftChild 
@@ -42,14 +44,15 @@ def inOrder(root): #left-> root ->right
           R - RightChild  
   '''
 
-def postOrder(root): #left -> right -> root
-  if not root:
-    return
-  preOrder(root.left)
-  preOrder(root.right)
-  print(root.data)
-  
-  '''
+
+def postOrder(root):  # left -> right -> root
+    if not root:
+        return
+    preOrder(root.left)
+    preOrder(root.right)
+    print(root.data)
+
+    '''
   --Postorder--
 
           LeftChild
@@ -60,10 +63,7 @@ def postOrder(root): #left -> right -> root
           R - RightChild
           Root                                --Root
   '''
-  
-  
-  
-  
+
 
 rootNode = TreeNode("Root")
 
@@ -75,13 +75,13 @@ rc = TreeNode("RightChild")
 rlc = TreeNode("R - Leftchild")
 rrc = TreeNode("R - RightChild")
 
-rootNode.left=lc
-rootNode.right=rc
+rootNode.left = lc
+rootNode.right = rc
 
-lc.left  = llc
+lc.left = llc
 lc.right = lrc
 
-rc.left  = rlc
+rc.left = rlc
 rc.right = rrc
 
 print("--Prorder--")
